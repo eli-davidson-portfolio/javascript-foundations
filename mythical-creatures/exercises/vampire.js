1ad8b987832dd6eb3b12ce1042d126c370db8505
Eli Davidson <eli@shadowlab.dev>
2022-02-07 19:11:14 -0700

    Update vampire file

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,22 +1,21 @@
 class Vampire {
   constructor(name, pet) {
       this.name = name;
-      if(pet === undefined) {
-          this.pet = 'bat';
-      } else {
-          this.pet = pet;
-      }
+      this.pet = 'bat';
       this.thirsty = true;
       this.ouncesDrank = 0;
+      if (pet !== undefined) {
+          this.pet = pet;
+      }
   }
-  
+
   drink() {
       if(this.ouncesDrank < 50) {
           this.ouncesDrank = this.ouncesDrank + 10;
-      } else {
-        return 'I\'m too full to drink anymore!';
+          return
+      } return 'I\'m too full to drink anymore!';
+      this.thirsty = (this.ouncesDrank === 0);
     }
-    this.thirsty = (this.ouncesDrank === 0); 
   }
 
 }
