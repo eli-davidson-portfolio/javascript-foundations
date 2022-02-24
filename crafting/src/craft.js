887a2b1a4ec6a8842e592357d139e6ab3017e419
Eli Davidson <eli@shadowlab.dev>
2022-02-23 21:11:57 -0700

    Complete Crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,20 @@
 class Craft {
-
+    constructor (craft) {
+        this.name = craft.type;
+        this.materials = craft.materials;
+        this.completed = false;
+    }
+    completeCraft(){
+        this.completed = true;
+        return `All done! It looks great!`
+    }
+    calculateProjectTotal() {
+        var projectTotal = 0;
+        for (var i = 0; i < this.materials.length; i++) {
+            projectTotal += this.materials[i].calculateMaterialCost();
+        }
+        return projectTotal;
+    }
 }
 
 module.exports = Craft;
