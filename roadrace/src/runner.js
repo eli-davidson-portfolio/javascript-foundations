187824a217cfadb4f233ed1c5e545c6257a4780f
Eli Davidson <eli@shadowlab.dev>
2022-02-23 22:16:40 -0700

    Complete roadrace

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,4 +1,23 @@
 class Runner {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+        this.milesRun = 0;
+        this.fitness = 25;
+        this.completedRaces = [];
+    }
+    runSomeMiles(miles) {
+        this.milesRun += miles;
+        this.fitness += miles;
+    }
+    stretch() {
+        this.fitness += 0.5;
+    }
+    runRace(name, miles) {
+        this.completedRaces.unshift(name);
+        this.runSomeMiles(miles);
+
+    }
 
 }
 
