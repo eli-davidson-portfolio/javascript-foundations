7d98648fbf43c7cd7ad6a255fda2bca9ce1a8ab5
Eli Davidson <eli@shadowlab.dev>
2022-02-23 21:39:27 -0700

    Complete do you wanan build a snowman

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,31 @@
 var Snowman = require('./Snowman');
 
 class Human {
+    constructor (name) {
+        this.name = name;
+        this.wantsToBuildASnowman = true;
+        this.materials = {
+            snowballs: 0,
+            coal: 0,
+            buttons: 0,
+            carrots: 0
+        }; 
+    }
+    gatherMaterials(name, amount) {
+        this.materials[name] += amount;
 
+    }
+    buildASnowman(){
+        return new Snowman(this.materials)
+    }
+    placeMagicHat(){
+        if (this.materials.coal >= 2 && this.materials.buttons >= 5 && this.materials.carrots && this.materials.snowballs >= 2) {
+            return `Woah, this snowman is coming to life!`;
+        } else {
+            return `I guess I didn\'t build it correctly.`
+        }
+    }
 }
+
 
 module.exports = Human;
